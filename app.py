7c66dc04fb930262c11c9ef4e4c56ce57d368469
bk376 <bk376@cornell.edu>
2020-01-19 02:32:12 -0800

    app - add initial app setup

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Ben',
+        'title': 'The design journey',
+        'content': 'This is  the initial stages of the ultimate blog',
+        'date_posted': 'January 20,  2020'
+    },
+    {
+        'author': 'Light Switch',
+        'title': 'The other post in design journey',
+        'content': 'This is  the initial stages of the ultimate blog and flask power',
+        'date_posted': 'January 20,  2020'
+    }
+]
+
+
+@app.route('/')
+def hello_world():
+    return render_template('home.html', posts=posts)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
