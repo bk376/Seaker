500e8dcc812acd1c2836fae989ff5daef1216c23
bk376 <bk376@cornell.edu>
2020-01-30 05:54:31 -0800

    auth - add user login

--- seaker/__init__.py
+++ seaker/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '63362f584ff66d1bda0281f6520bc914'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///seaker.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from seaker import routes
