2cfeda5d20a841ee34e11469a412a7c367aa74d9
bk376 <bk376@cornell.edu>
2020-01-30 06:24:56 -0800

    route| auth - add routing and template for user account page

--- seaker/routes.py
+++ seaker/routes.py
@@ -1,5 +1,5 @@
-from flask import render_template, redirect, url_for, flash
-from flask_login import login_user, current_user, logout_user
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_user, current_user, logout_user, login_required
 
 from seaker import app, db, bcrypt
 from seaker.forms import RegistrationForm, LoginForm
@@ -54,8 +54,9 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
+            next_page = request.args.get('next')
             login_user(user, remember=form.remember.data)
-            return redirect(url_for("home"))
+            return  redirect(next_page) if next_page else redirect(url_for("home"))
         else:
             flash("Login unsuccessful. Please check email and password", 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -65,3 +66,8 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("home"))
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html', title='Account')
