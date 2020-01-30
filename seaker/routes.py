c32a836fe2487b5a66af23e986c2f1075d0711d7
bk376 <bk376@cornell.edu>
2020-01-30 07:36:23 -0800

    route - handle account update form submissions

--- seaker/routes.py
+++ seaker/routes.py
@@ -1,8 +1,10 @@
+import secrets
+import os
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_user, current_user, logout_user, login_required
 
 from seaker import app, db, bcrypt
-from seaker.forms import RegistrationForm, LoginForm
+from seaker.forms import RegistrationForm, LoginForm, AccountUpdateForm
 from seaker.models import User
 
 posts = [
@@ -67,7 +69,30 @@ def logout():
     logout_user()
     return redirect(url_for("home"))
 
-@app.route('/account')
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    # get name and extension for the picture file uploaded
+    f_name, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics',picture_fn)
+    form_picture.save(picture_path)
+    return  picture_fn
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = AccountUpdateForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            piture_file = save_picture(form.picture.data)
+            current_user.image_file = piture_file
+        current_user.name = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/'+ current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
