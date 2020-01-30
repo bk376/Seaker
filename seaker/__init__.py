d80f52d7e96a2a89f9364842212f72dfc47e93ed
bk376 <bk376@cornell.edu>
2020-01-30 05:33:58 -0800

    add database validation for form registration inputs

--- seaker/__init__.py
+++ seaker/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '63362f584ff66d1bda0281f6520bc914'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///seaker.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from seaker import routes
--- seaker/routes.py
+++ seaker/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, redirect, url_for, flash
 
-from seaker import app
+from seaker import app, db, bcrypt
 from seaker.forms import RegistrationForm, LoginForm
+from seaker.models import User
 
 posts = [
     {
@@ -33,8 +34,12 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Your account has been created! You are now able to login', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 
