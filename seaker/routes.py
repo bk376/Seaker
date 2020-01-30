2feaef3fcbd1861e4bfc887b8f4b1c752dedcafa
bk376 <bk376@cornell.edu>
2020-01-30 07:54:40 -0800

    resize image before saving to control size

--- seaker/routes.py
+++ seaker/routes.py
@@ -1,5 +1,6 @@
 import secrets
 import os
+from PIL import Image
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -75,7 +76,11 @@ def save_picture(form_picture):
     f_name, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics',picture_fn)
-    form_picture.save(picture_path)
+    # resize image using Pillow befor saving
+    output_size = (125, 125)
+    img = Image.open(form_picture)
+    img.thumbnail(output_size)
+    img.save(picture_path)
     return  picture_fn
 
 @app.route('/account', methods=['GET', 'POST'])
